d42e7f8504e781a6884982d06f3e7b4c959af6b0
Enzo-Arilla <enzoarilla8@gmail.com>
2023-10-10 19:48:41 -0300

    Manipulando o objeto do tipo ResponseEntity do Spring.

--- todolist/src/main/java/br/com/enzoarilla/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/enzoarilla/todolist/user/UserController.java
@@ -1,10 +1,14 @@
 package br.com.enzoarilla.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.ipc.http.HttpSender.Response;
 
 @RestController
 @RequestMapping("/users")
@@ -14,8 +18,17 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+       var user = this.userRepository.findByUsername(userModel.getUsername());
+
+       if (user != null) {
+            System.out.println("Usuário já existe!");
+            //Mensagem de erro
+            //Status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+       }
+
        var userCreated = this.userRepository.save(userModel);
-       return userCreated;
+       return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
