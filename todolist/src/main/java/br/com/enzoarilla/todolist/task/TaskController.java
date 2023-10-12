affe587bd0602d1e969c79589c0dc4da5e456a3b
Enzo-Arilla <enzoarilla8@gmail.com>
2023-10-11 23:16:06 -0300

    Adicionando a classe 'Filter' para realizar validações dos dados para o banco de dados.

--- todolist/src/main/java/br/com/enzoarilla/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/enzoarilla/todolist/task/TaskController.java
@@ -1,6 +1,7 @@
 package br.com.enzoarilla.todolist.task;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class TaskController {
     @Autowired
     private ITaskRepository taskRepository;
 
+    @PostMapping
     public TaskModel create(@RequestBody TaskModel taskModel) {
         var task = this.taskRepository.save(taskModel);
         return task;
